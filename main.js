2c6f975eff794489d0998f7cca392619bc8f8f44
falcon102582 <falcon102582@gmail.com>
2022-12-27 12:56:20 +0800

    add ai play mode

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ const grids = document.querySelectorAll(".grid");
 const result = document.querySelector("#result");
 const reminder = document.querySelector("#reminder");
 const reset = document.querySelector("#reset");
+const playAiBtn = document.querySelector("#playAi");
+const playHumanBtn = document.querySelector("#playHuman");
 
 let count = 0;
 let xList = [];
 let oList = [];
+let game = false;
+let gameOver = false;
+let ai = false;
 
 const winFormula = [
     [0,1,2],
@@ -18,35 +23,47 @@ const winFormula = [
     [2,4,6]
 ]
 
+let remainingGirds = [0,1,2,3,4,5,6,7,8];
+
 
 for(let i = 0; i < grids.length; i++){
     grids[i].addEventListener("click", ()=>{
-        if(grids[i].textContent !== '' || reminder.textContent === "Congratulations! O win the game!" || reminder.textContent === "Congratulations! X win the game!"){
+        if(grids[i].textContent !== '' || gameOver === true || game === false){
             return true
         }
         if(count%2 === 0){
             count++;
             grids[i].innerHTML = "O";
-            reminder.textContent = "It's X turn!"
+            reminder.textContent = "It's X turn!";
+            remainingGirds.splice(remainingGirds.indexOf(i), 1);
             oList.push(i);
             if(checkWin(oList)){
+                gameOver = true;
                 reminder.textContent = "Congratulations! O win the game!"
                 reset.style.backgroundColor = "red";
             }
         }else{
             count++;
             grids[i].innerHTML = "X";
+            remainingGirds.splice(remainingGirds.indexOf(i), 1);
             xList.push(i);
             reminder.textContent = "It's O turn!"
             if(checkWin(xList)){
+                gameOver = true;
                 reminder.textContent = "Congratulations! X win the game!"
                 reset.style.backgroundColor = "red";
             }
         }
+
+        if(ai === true && gameOver ===false){
+            setTimeout(()=> computerPlay(), 1000) ;
+        }
     })
 }
 
-reset.addEventListener("click", () =>{
+reset.addEventListener("click", resetGame);
+
+function resetGame(){
     for(const grid of grids){
         grid.textContent = '';
     }
@@ -55,7 +72,9 @@ reset.addEventListener("click", () =>{
     reset.style.backgroundColor = "";
     oList = [];
     xList = [];
-})
+    gameOver = false;
+    remainingGirds = [0,1,2,3,4,5,6,7,8];
+}
 
 function check(arr, target){
     return target.every(v => arr.includes(v))
@@ -66,5 +85,74 @@ function checkWin(arr){
         if(check(arr, formula)){
             return true
         }
+    }
+    if(remainingGirds.length === 0){
+        reminder.innerHTML = "Draw!"
+        gameOver = true;
+    }
+}
+// choose mode
+playHumanBtn.addEventListener("click", ()=>{
+    resetGame();
+    playHumanBtn.style.border = "black solid 3px";
+    playHumanBtn.style.fontWeight = "900";
+    playAiBtn.style.border = "";
+    playAiBtn.style.fontWeight = "";
+    ai=false
+    game = true;
+})
+
+playAiBtn.addEventListener("click", ()=>{
+    resetGame();
+    playAiBtn.style.border = "black solid 3px";
+    playAiBtn.style.fontWeight = "900";
+    playHumanBtn.style.border = "";
+    playHumanBtn.style.fontWeight = "";
+    ai = true;
+    game = true;
+})
+// computerplay
+function random(num){
+    return Math.floor(Math.random()*num);
+}
+// easy mode
+// function computerPlay(){
+//     let decision = remainingGirds[random(remainingGirds.length)];
+//     count++;
+//     grids[decision].innerHTML = "X";
+//     remainingGirds.splice(remainingGirds.indexOf(decision), 1);
+//     xList.push(decision);
+//     reminder.textContent = "It's O turn!"
+//     if(checkWin(xList)){
+//         reminder.textContent = "Congratulations! X win the game!"
+//         reset.style.backgroundColor = "red";
+//     }
+// }
+
+
+
+function computerPlay(){
+    let decision = remainingGirds[random(remainingGirds.length)];
+    for(const i of remainingGirds){
+        oList.push(i);
+        xList.push(i);
+        if(checkWin(xList)){
+         decision = i;
+         break;
+        }else if(checkWin(oList)){
+         decision = i;
+        }
+        oList.pop();
+        xList.pop();
+    }
+    count++;
+    grids[decision].innerHTML = "X";
+    remainingGirds.splice(remainingGirds.indexOf(decision), 1);
+    xList.push(decision);
+    reminder.textContent = "It's O turn!"
+    if(checkWin(xList)){
+        gameOver = true;
+        reminder.textContent = "Congratulations! X win the game!"
+        reset.style.backgroundColor = "red";
     }
 }
